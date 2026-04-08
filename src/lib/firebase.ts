d8a5d2dc58b3675bc99b605eb93992865a431bff
madisonlaport-lang <madison.laport@yahoo.com>
2026-04-08 18:44:15 +0200

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,13 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDjMEOjAEWvNLX0UQEgdiuMXEHiEKZFnAA",
+  authDomain: "invento-3b3c3.firebaseapp.com",
+  projectId: "invento-3b3c3",
+  storageBucket: "invento-3b3c3.firebasestorage.app",
+  messagingSenderId: "254575165027",
+  appId: "1:254575165027:web:a016de33295f4843320256",
+  measurementId: "G-QJG44DT427"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
+const analytics = getAnalytics(app);
