396b0ae9bdcc2cddd6ff6945839947b1df6861cf
madisonlaport-lang <madison.laport@yahoo.com>
2026-04-09 21:00:42 +0200

    Update AuthContext.tsx

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -117,7 +117,10 @@ export function AuthProvider({ children }: { children: ReactNode }) {
   email: string
 ): Promise<{ success: boolean; error?: string }> => {
   try {
-    await sendPasswordResetEmail(auth, email);
+   await sendPasswordResetEmail(auth, email, {
+     url: "https://kenwa-conciergerie.com/login",
+     handleCodeInApp: false,
+   });
     return { success: true };
   } catch (error: any) {
     console.error("ERREUR FORGOT PASSWORD :", error);
