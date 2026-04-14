b939d5123c01c59085bd86fd1ee8054f3846ab7b
madisonlaport-lang <madison.laport@yahoo.com>
2026-04-14 12:18:37 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useAuth } from '@/contexts/AuthContext';
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./router";
 import { I18nextProvider } from "react-i18next";
@@ -6,7 +7,19 @@ import { AuthProvider } from "./contexts/AuthContext";
 import { InventoryProvider } from "./contexts/InventoryContext";
 
 function App() {
+  const { isLoading } = useAuth();
+
+  // ⛔ BLOQUE l'app pendant le chargement Firebase
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        Chargement...
+      </div>
+    );
+  }
+
   return (
+    // ton app normale ici (
     <I18nextProvider i18n={i18n}>
       <BrowserRouter basename={__BASE_PATH__}>
         <AuthProvider>
